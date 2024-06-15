8cbd9bf13331b97e65c4090f8c57fa21c9e325a3
Zeinab-kr <zeinab_kr@aut.ac.ir>
2024-06-15 22:41:06 +0330

    cli.py initialized

---
+++ cli.py
@@ -0,0 +1,17 @@
+import actions
+
+
+def display_menu():
+    print("1. Add Student")
+    print("2. Remove Student")
+    print("3. Charge Balance")
+    print("4. Add Food")
+    print("5. Remove Food")
+    print("6. Make Reservation")
+    print("7. View All Students")
+    print("8. View All Foods")
+    print("9. View All Reservations")
+    print("10. Cancel Reservation")
+    print("11. Change Reservation")
+    print("12. Exit")
+
